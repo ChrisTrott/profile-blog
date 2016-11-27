88d7428a7a09bdc7790858c733562857327ab6ee
Chris Trott <Chris Trott>
2016-11-27 23:01:19 +1100

    Initial site file added

---
+++ client/components/layout/app.jsx
@@ -0,0 +1,56 @@
+App = React.createClass({
+  mixins: [ ReactMeteorData ],
+  getMeteorData() {
+    return {
+      loggingIn: Meteor.loggingIn(),
+      hasUser: !!Meteor.user(),
+      isPublic( route ) {
+        return [
+          'index',
+          'singlePost',
+          'tagIndex',
+          'login',
+          'recoverPassword',
+          'resetPassword',
+          'notFound'
+        ].indexOf( route ) > -1;
+      },
+      canView() {
+        return this.isPublic( FlowRouter.getRouteName() ) || !!Meteor.user();
+      }
+    };
+  },
+  renderHeader() {
+    if ( this.props.tag ) {
+      return <Jumbotron className="tags-header">
+        <h4>Posts tagged with: { this.props.tag }.</h4>
+      </Jumbotron>;
+    } else {
+      return <Jumbotron className="blog-header">
+          <h2>Chris Trott</h2>
+          <h4>I'm the Product Manager at a SaaS company where I manage,
+            develop, deploy, and maintain web applications.</h4>
+          <h4>
+            I enjoy teaching people about technology, and love producing
+            beautiful software that improves people's lives.
+          </h4>
+      </Jumbotron>;
+    }
+  },
+  getView() {
+    return this.data.canView() ? this.props.yield : <Login />;
+  },
+  render() {
+    return <div className="app-root">
+      <AppHeader hasUser={ this.data.hasUser } />
+      <div className="app-view">
+        <div className="banner-head">
+          { this.renderHeader() }
+        </div>
+        <div className="container">
+          { this.data.loggingIn ? <Loading /> : this.getView() }
+        </div>
+      </div>
+    </div>;
+  }
+});
---
+++ client/components/views/login.jsx
@@ -0,0 +1,67 @@
+Login = React.createClass({
+  validations() {
+    let component = this;
+
+    return {
+      rules: {
+        emailAddress: {
+          required: true,
+          email: true
+        },
+        password: {
+          required: true
+        }
+      },
+      messages: {
+        emailAddress: {
+          required: 'You need to specify an email address.',
+          email: 'Is this email address legit?'
+        },
+        password: {
+          required: 'Need a password here.'
+        }
+      },
+      submitHandler() {
+        let { getValue } = ReactHelpers;
+
+        let form     = component.refs.loginForm.refs.form,
+            email    = getValue( form, '[name="emailAddress"]' ),
+            password = getValue( form, '[name="password"]' );
+
+        Meteor.loginWithPassword( email, password, ( error ) => {
+          if ( error ) {
+            Bert.alert( error.reason, 'danger' );
+          } else {
+            Bert.alert( 'Logged in!', 'success' );
+          }
+        });
+      }
+    };
+  },
+  handleSubmit( event ) {
+    event.preventDefault();
+  },
+  render() {
+    let passwordLabelLink = {
+      href: '/recover-password',
+      label: 'Forgot Your Password?'
+    };
+
+    return <GridRow>
+      <GridColumn className="col-xs-12 col-sm-6 col-md-5 col-lg-4">
+        <PageHeader size="h4" label="Log In" />
+        <Form ref="loginForm" id="login" className="login" validations={ this.validations() } onSubmit={ this.handleSubmit }>
+          <FormGroup>
+            <EmailInput ref="emailAddress" showLabel={ true } />
+          </FormGroup>
+          <FormGroup>
+            <PasswordInput ref="password" showLabel={ true } labelLink={ passwordLabelLink } />
+          </FormGroup>
+          <FormGroup>
+            <SuccessButton type="submit" label="Login" />
+          </FormGroup>
+        </Form>
+      </GridColumn>
+    </GridRow>;
+  }
+});
